8adfc6f257fd9e4a4a722683c43a59c9d5f87a50
thanhhuy5902 <tranthanhhuy42@gmail.com>
2024-09-14 13:30:01 +0700

    init scan NFC

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_projects/presentation/screen/read_write_nfc_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NFC MANAGER',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ReadWriteNFCScreen()
+    );
+  }
+}
